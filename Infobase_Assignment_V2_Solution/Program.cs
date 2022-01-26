6c1ea7b9b33fee927032434c2e7a9b4bd3f90a45
ADMIN <ADMIN@DESKTOP-PP9IE1Q>
2022-01-26 17:10:50 +0530

    Add project files.

---
+++ Infobase_Assignment_V2_Solution/Program.cs
@@ -0,0 +1,38 @@
+﻿class WordSequenceGenerator
+{
+	private static readonly int FOUR = 4;
+
+	public static void Main()
+	{
+		try
+		{
+			SortedDictionary<string, string> sortedUniqueKeysAndValues = new();
+			foreach (string line in File.ReadLines(@"Resources\words.txt"))
+			{
+				int strLength = line.Length;
+				if (strLength <= FOUR && !sortedUniqueKeysAndValues.ContainsKey(line))
+				{
+					sortedUniqueKeysAndValues.Add(line, line);
+				}
+				else
+				{
+					for (int i = 0; i <= strLength - FOUR; i++)
+					{
+						string subStr = line.Substring(i, FOUR);
+						if (!sortedUniqueKeysAndValues.ContainsKey(subStr))
+						{
+							sortedUniqueKeysAndValues.Add(subStr, line);
+						}
+					}
+				}
+			}
+			File.WriteAllLines(@"Resources\uniques.txt", sortedUniqueKeysAndValues.Keys);
+			File.WriteAllLines(@"Resources\fullwords.txt", sortedUniqueKeysAndValues.Values);
+		}
+		catch(Exception ex)
+        {
+			Console.WriteLine("Exception Message: " + ex.Message);
+        }
+	}
+}
+
